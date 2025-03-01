61200916fc4d3e36ea9a9c6f936769f92e9daf19
Ayush <siayush.gupta@gmail.com>
2025-03-01 23:49:31 +0530

    repl for function

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -80,6 +80,10 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		if isError(function) {
 			return function
 		}
+		args := evalExpressions(node.Arguments, env)
+		if len(args) == 1 && isError(args[0]) {
+			return args[0]
+		}
 	}
 
 	return nil
@@ -239,3 +243,5 @@ func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object
 	}
 	return val
 }
+
+func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Object {}
